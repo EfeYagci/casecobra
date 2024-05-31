b0c9a864a8093faf9005bda1243063d3a25abbe8
EfeYagci <yagciefe@gmail.com>
2024-05-31 18:44:02 +0300

    first vercel deploy

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,37 @@
-const Page = async () => {
-  // make db call
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
 
-  return <p></p>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
- 
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
+};
+
 export default Page;
