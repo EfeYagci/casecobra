3f448baad5a77b08468440fe85b434b7651326f0
EfeYagci <yagciefe@gmail.com>
2024-06-02 13:22:02 +0300

    Final version

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://casecobra.vercel.app'
+      : 'https://casecobra-khaki.vercel.app/'
 
   return (
     <Html>
